95c952bf431660b955d99c2c64546a4a3fcb3442
Thekob <thekob.mx@gmail.com>
2018-10-29 09:33:48 +0700

    Var and constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//นี่คือการประกาศตัวแปรแบบ Implicit
+
+var nameString = "MasterUNG"    //นี่คือการประกาศ nameString ที่มีค่า MasterUNG เป็น String Type
+
+var number1Int = 100
+var number2Dou = 12.34
+var status1Bool = true
+
+//Explicit Type
+
+var name2String: String = "Doraemon" //Explicit คือการประกาศแบบ กำหนด Data Type
+var nuber3Int: Int = 123
+var status2Bool: Bool = true
+var number4Dou: Double = 123.45
+
+print("Current NameString ==> \(nameString)")
+nameString = "Nobita"
+print("Current NameString ==> \(nameString)")
+
+
+//การประกาศค่าคงที่
+let surnameString: String = "Phetwong"
+//surnameString = "AAAA"  ไม่สามารถ Assign Value ได้
+
+
+
+
+
+
+
+
+
